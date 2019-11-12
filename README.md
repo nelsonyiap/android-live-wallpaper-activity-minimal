afd9e9602e5bccba0e00cacc32252c45f395c027
Nelson <nelson.yiap@gmail.com>
2019-11-12 16:18:43 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# android-live-wallpaper-activity-minimal
-Minimal demo of an Android live wallpaper launched by an activity
+# Live Wallpaper set by an Android Activity Minimal example
+
+Demo project of a working live wallpaper set via Intent 
+
+Based on the following video example:
+[YouTube video to develop a simple live wallpaper service - no activity](https://www.youtube.com/watch?v=Tym8lRg72GA)
---
+++ app/src/main/java/com/nksy/livewallpapertutorial/TutorialWallpaperService.java
@@ -0,0 +1,107 @@
+package com.nksy.livewallpapertutorial;
+
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Handler;
+import android.service.wallpaper.WallpaperService;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.SurfaceHolder;
+
+public class TutorialWallpaperService extends WallpaperService {
+
+    private static final long TOUCH_DURATION = 1000;
+    private static final long FRAME_RATE = (long)(1/60f * 1000);
+    private final Paint mPaint = new Paint();
+    private final Handler handler = new Handler();
+    private long touchStart;
+    private float touchx;
+    private float touchy;
+
+    @Override
+    public Engine onCreateEngine() {
+        return new TutorialWallpaperEngine();
+    }
+
+    private class TutorialWallpaperEngine extends WallpaperService.Engine {
+
+        private final Runnable mDraw = new Runnable() {
+            @Override
+            public void run() {
+                drawFrame();
+            }
+        };
+
+        public TutorialWallpaperEngine() {
+
+        }
+
+        @Override
+        public void onCreate(SurfaceHolder surfaceHolder) {
+            super.onCreate(surfaceHolder);
+            setTouchEventsEnabled(true);
+            mPaint.setColor(Color.WHITE);
+            mPaint.setAntiAlias(true);
+            mPaint.setAlpha(150);
+            handler.post(mDraw);
+        }
+
+        @Override
+        public void onDestroy() {
+            super.onDestroy();
+            handler.removeCallbacks(mDraw);
+        }
+
+        @Override
+        public void onTouchEvent(MotionEvent event) {
+            Log.d("TouchEvent", "Touched at " +event.getX() + ", " + event.getY());
+            touchStart = System.currentTimeMillis();
+            touchx = event.getX();
+            touchy = event.getY();
+        }
+
+        private void drawFrame() {
+            final SurfaceHolder surfaceHolder = getSurfaceHolder();
+            Canvas canvas = null;
+            try {
+                canvas = surfaceHolder.lockCanvas();
+                if (canvas != null) {
+                    Log.d("drawFrame", "Drawing Something!");
+                    canvas.drawColor(Color.RED);
+                    drawCircle(canvas);
+                }
+            } finally {
+                if (canvas != null) {
+                    surfaceHolder.unlockCanvasAndPost(canvas);
+                }
+            }
+
+            handler.removeCallbacks(mDraw);
+            handler.postDelayed(mDraw, FRAME_RATE);
+        }
+
+        private void drawCircle(final Canvas canvas) {
+            Log.d("drawCircle", "Trying to draw circle");
+            if (System.currentTimeMillis() > touchStart + TOUCH_DURATION) {
+                Log.d("drawCircle", "NO NEED TO Circle drawn");
+                return;
+            }
+            float t = (float)(System.currentTimeMillis() - touchStart) / TOUCH_DURATION;
+            float interpolation = interpolation(t);
+            Log.d("drawCircle", "Circle drawn w alpha interp " + interpolation);
+            mPaint.setAlpha((int)(150 * interpolation));
+            canvas.drawCircle(touchx, touchy, 50, mPaint);
+        }
+
+        private float interpolation(final float t) {
+            if (t < 0) {
+                return 0;
+            } else if (t > 1) {
+                return 1;
+            }
+            return 1-t*t*t;
+        }
+    }
+
+}
